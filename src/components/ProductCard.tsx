ca37245d649e87d28d34674a3cb0446d509a5b0f
Brian Walters <brian.walters@nearform.com>
2024-02-09 11:38:10 -0600

    Add a product card

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,17 @@
+import {FunctionComponent} from "react";
+
+interface ProductCardProps {
+  id: string
+  name: string
+  image: string
+}
+
+export const ProductCard:FunctionComponent<ProductCardProps> = ({ name, id, image}) => {
+  return (
+    <div className="border rounded p4">
+      <a className="text-2xl" href={`/product/${id}`}>{name}</a>
+      <p>{name}</p>
+      <p><img src={image} alt=""/></p>
+    </div>
+  );
+};
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,6 +3,7 @@
 import {FunctionComponent, useEffect, useState} from "react";
 import {viewModel} from "@/ViewModel";
 import {ProductViewModel} from "@/ViewModel/types/ProductViewModel";
+import {ProductCard} from "@/components/ProductCard";
 
 export const ProductList: FunctionComponent = () => {
   const [products, setProducts] = useState<ProductViewModel[]>([])
@@ -17,12 +18,10 @@ export const ProductList: FunctionComponent = () => {
         <summary>Data</summary>
         <pre>{JSON.stringify(products, null, 1)}</pre>
       </details>
-      <ul>
+      <ul className="grid gap-4 grid-cols-[repeat(auto-fill,_minmax(240px,_1fr))]">
         {products.length > 0 && products.map((product) => (
           <li key={product.id}>
-            <a className="text-2xl" href={`/product/${product.id}`}>{product.name}</a>
-            <p>{product.name}</p>
-            <p><img src="https://placehold.co/600x400" alt=""/></p>
+            <ProductCard id={product.id} name={product.name} image="https://placehold.co/600x400" />
           </li>
         ))}
       </ul>
