6533ef145ef6bc8b6cde6f59d757fac761b9cf40
Brian Walters <brian.walters@nearform.com>
2024-02-13 13:57:05 -0600

    Need different env vars for client id and client secret

--- src/Commerce/index.ts
+++ src/Commerce/index.ts
@@ -7,7 +7,7 @@ export const commerceRepository = new CommerceRepository(
 )
 
 export const commerceSyncer = new CommerceSyncer(
-  process.env.NEXT_PUBLIC_COMMERCE_LAYER_CLIENT_ID!,
-  process.env.COMMERCE_LAYER_CLIENT_SECRET!,
+  process.env.COMMERCE_LAYER_INTEGRATION_CLIENT_ID!,
+  process.env.COMMERCE_LAYER_INTEGRATION_CLIENT_SECRET!,
   process.env.NEXT_PUBLIC_COMMERCE_LAYER_BASE_ENDPOINT!
 )
